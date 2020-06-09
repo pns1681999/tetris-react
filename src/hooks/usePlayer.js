0c85722402589d57c68e261eca781d72d4e88011
dongtaduocsu <55840736+dongtaduocsu@users.noreply.github.com>
2020-06-09 21:11:18 +0700

    Player RotationG

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,13 +1,44 @@
 import {useState, useCallback} from 'react';
-import { randomTetromino} from '../tetrominos';
-import { STAGE_HEIGHT, STAGE_WIDTH } from '../gameHelpers';
+import {TETROMINOS, randomTetromino} from '../tetrominos';
+import { STAGE_HEIGHT, STAGE_WIDTH,checkCollision } from '../gameHelpers';
 
 export const usePlayer=()=>{
     const [player,setPlayer]=useState({
         pos:{x:0,y:0},
-        tetromino:randomTetromino().shape,
+        tetromino:TETROMINOS[0].shape,
         collided:false,
     });
+
+    const rotate=(matrix,dir)=>{
+        const rotatedTetro=matrix.map((_,index)=>matrix.map(col=>col[index]),);
+        if(dir>0) return rotatedTetro.map(row=>row.reverse());
+        return rotatedTetro.reverse();
+    };
+
+    const playerRotate=(stage ,dir )=>{
+        const clonePlayer=JSON.parse(JSON.stringify(player));
+        clonePlayer.tetromino=rotate(clonePlayer.tetromino,dir);
+
+
+
+        const pos=clonePlayer.pos.x;
+        let offset=1;
+        while(checkCollision(clonePlayer,stage,{x:0,y:0}))
+        {
+            clonePlayer.pos.x+=offset;
+            offset-=(offset+(offset>0?1:-1));
+             if(offset>clonePlayer.tetromino[0].length){
+                 rotate(clonePlayer.tetromino,-dir);
+                 clonePlayer.pos.x=pos;
+                 return ;
+             }
+
+        }
+
+        setPlayer(clonePlayer);
+    }
+     
+
     const updatePlayerPos=({x,y,collided})=>{
         setPlayer(prev=>({
             ...prev,
@@ -25,6 +56,6 @@ export const usePlayer=()=>{
 },[])
      
 
-    return [player,updatePlayerPos,resetPlayer];
+    return [player,updatePlayerPos,resetPlayer,playerRotate];
     
 }
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -2,9 +2,8 @@ import {useState,useEffect} from 'react';
 import { createStage} from '../gameHelpers';
 
 export const useStage=(player,resetPlayer)=>{
-    const [stage,setStage]=useState(
-       createStage()
-    );
+    const [stage,setStage]=useState(createStage() );
+    
     useEffect(()=>{
     const updateStage=prevStage=>{
         const newStage=prevStage.map(row=>row.map(cell=>(cell[1]==='clear'?[0,'clear']:cell)),
@@ -21,12 +20,18 @@ export const useStage=(player,resetPlayer)=>{
                 }
             });
         });
+    if(player.collided)
+    {
+        
+        resetPlayer();
+    }
+
         return newStage;
 
     }; 
-    setStage(prev=>updateStage(prev))
+    setStage(prev=>updateStage(prev));
 
-    },[player.collided,player.pos.x,player.pos.y,player.tetromino]);
+    },[player,resetPlayer]);
     return [stage,setStage];
     
 }
