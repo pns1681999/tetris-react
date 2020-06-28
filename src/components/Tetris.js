d28e99ef6b93e6f7d5d34b9cd1820b41570392f3
pns1681999 <43614413+pns1681999@users.noreply.github.com>
2020-06-28 19:21:28 +0700

    drop

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,127 +1,128 @@
-import React,{useState} from 'react';
-import { createStage,checkCollision} from '../gameHelpers';
+import React, { useState } from "react";
+import { createStage, checkCollision } from "../gameHelpers";
 
-import { StyledTetrisWrapper,StyledTetris} from './styles/StyledTetris';
+import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 //custom hooks
-import {useInterval} from '../hooks/useInterval';
-import {usePlayer}from '../hooks/usePlayer';
-import {useStage} from '../hooks/useStage';
-import {useGameStatus} from '../hooks/useGameStatus';
+import { useInterval } from "../hooks/useInterval";
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+import { useGameStatus } from "../hooks/useGameStatus";
 
 //components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
-
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
 
 const Tetris = () => {
-    // eslint-disable-next-line
-    const [dropTime,setDropTime]=useState(null);
-    const [gameOver,setGameOver]=useState(false);
+  // eslint-disable-next-line
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
 
-    const [player,updatePlayerPos,resetPlayer,playerRotate]=usePlayer();
-    const [stage,setStage, rowsCleared]=useStage(player,resetPlayer);
-    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
-    
-    console.log('re-render');
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(
+    rowsCleared
+  );
 
+  console.log("re-render");
 
-    const movePlayer=dir=>{
-        if(!checkCollision(player,stage,{x:dir,y:0})){
-        updatePlayerPos({x:dir ,y:0});
-        }
+  const movePlayer = (dir) => {
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
     }
-    const startGame=()=>{
-        console.log("test");
-        setStage(createStage());
-        setDropTime(1000);
-        resetPlayer();
-        setGameOver(false);
-        setScore(0);
-        setRows(0);
-        setLevel(0);
-
+  };
+  const startGame = () => {
+    console.log("test");
+    setStage(createStage());
+    setDropTime(1000);
+    resetPlayer();
+    setGameOver(false);
+    setScore(0);
+    setRows(0);
+    setLevel(0);
+  };
+  const drop = () => {
+    //Cleared 10 rows -> up level
+    if (rows > (level + 1) * 5) {
+      setLevel((prev) => prev + 1);
+      setDropTime(1000 / (level + 1) + 200);
     }
-    const drop=()=>{
-        //Cleared 10 rows -> up level
-        if (rows > (level+1)*5) {
-            setLevel(prev => prev+1);
-            setDropTime(1000/(level+1)+200);        
-        }
-        if(!checkCollision(player,stage,{x:0,y:1})){
-        updatePlayerPos({x:0,y:1,collided:false})
-        } else{
-            if(player.pos.y<1)
-            {
-                console.log("GAME OVER!!!");
-                setGameOver(true);
-                setDropTime(null);
-
-            }
-            updatePlayerPos({x:0,y:0,collided:true});
-        }
-    }
-
-    const keyUp = ({keyCode}) => {
-        if(!gameOver) {
-            if (keyCode === 40) {
-                setDropTime(1000/(level+1)+200);
-            }
-        }
-    }
-    const dropPlayer=()=>{
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      if (player.pos.y < 1) {
+        console.log("GAME OVER!!!");
+        setGameOver(true);
         setDropTime(null);
-      
-        drop();
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
     }
-    const move=({keyCode})=>{
-        if(!gameOver)
-        {
-            if(keyCode===37)
-            {
-                movePlayer(-1);
-            }
-            else if(keyCode===39)
-            {
-                movePlayer(1);
-            }
-            else if(keyCode===40)
-            {
-                dropPlayer();
-            }
-            else if(keyCode===38){
-                playerRotate(stage,1);
+  };
 
-            }
-
-
-        }
+  const dropNow = () => {
+    let i = 0;
+    while (!checkCollision(player, stage, { x: 0, y: i+1 })) {
+      i++;
     }
+    updatePlayerPos({ x: 0, y: i, collided: true });
+  };
 
-    useInterval(() => {
-        drop();
+  const keyUp = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 40) {
+        setDropTime(1000 / (level + 1) + 200);
+      }
+    }
+  };
+  const dropPlayer = () => {
+    setDropTime(null);
 
-    }, dropTime)
+    drop();
+  };
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
+      } else if (keyCode === 32) {
+        dropNow();
+      }
+    }
+  };
 
-    return (
+  useInterval(() => {
+    drop();
+  }, dropTime);
 
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e=>move (e)} onKeyUp={keyUp}>
-            <StyledTetris>
-                <Stage stage={stage} />
-                <aside>
-                    {gameOver?(<Display gameOver={gameOver } text="Game Over"/>
-                    ): (
-                    <div>
-                        <Display text={`Score: ${score}`} />
-                        <Display text={`Rows: ${rows}`} />
-                        <Display text={`Level: ${level}`} />
-                    </div>
-                    )}
-                     <StartButton callback ={startGame}/>
-                </aside>
-            </StyledTetris>
-        </StyledTetrisWrapper>
-    );
+  return (
+    <StyledTetrisWrapper
+      role="button"
+      tabIndex="0"
+      onKeyDown={(e) => move(e)}
+      onKeyUp={keyUp}
+    >
+      <StyledTetris>
+        <Stage stage={stage} />
+        <aside>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
+            </div>
+          )}
+          <StartButton callback={startGame} />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
+  );
 };
 
 export default Tetris;
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,62 +1,56 @@
-import {useState, useCallback} from 'react';
+import { useState, useCallback } from "react";
 
-import {TETROMINOS, randomTetromino} from '../tetrominos';
-import { STAGE_HEIGHT, STAGE_WIDTH,checkCollision } from '../gameHelpers';
+import { TETROMINOS, randomTetromino } from "../tetrominos";
+import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 
-export const usePlayer=()=>{
-    const [player,setPlayer]=useState({
-        pos:{x:0,y:0},
-        tetromino:TETROMINOS[0].shape,
-        collided:false,
+export const usePlayer = () => {
+  const [player, setPlayer] = useState({
+    pos: { x: 0, y: 0 },
+    tetromino: TETROMINOS[0].shape,
+    collided: false,
+  });
+
+  const rotate = (matrix, dir) => {
+    const rotatedTetro = matrix.map((_, index) =>
+      matrix.map((col) => col[index])
+    );
+    if (dir > 0) return rotatedTetro.map((row) => row.reverse());
+    return rotatedTetro.reverse();
+  };
+
+  const playerRotate = (stage, dir) => {
+    const clonePlayer = JSON.parse(JSON.stringify(player));
+    clonePlayer.tetromino = rotate(clonePlayer.tetromino, dir);
+
+    const pos = clonePlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonePlayer, stage, { x: 0, y: 0 })) {
+      clonePlayer.pos.x += offset;
+      offset -= offset + (offset > 0 ? 1 : -1);
+      if (offset > clonePlayer.tetromino[0].length) {
+        rotate(clonePlayer.tetromino, -dir);
+        clonePlayer.pos.x = pos;
+        return;
+      }
+    }
+
+    setPlayer(clonePlayer);
+  };
+
+  const updatePlayerPos = ({ x, y, collided }) => {
+    setPlayer((prev) => ({
+      ...prev,
+      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+      collided,
+    }));
+  };
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+      tetromino: randomTetromino().shape,
+      collided: false,
     });
+  }, []);
 
-    const rotate=(matrix,dir)=>{
-        const rotatedTetro=matrix.map((_,index)=>matrix.map(col=>col[index]),);
-        if(dir>0) return rotatedTetro.map(row=>row.reverse());
-        return rotatedTetro.reverse();
-    };
-
-    const playerRotate=(stage ,dir )=>{
-        const clonePlayer=JSON.parse(JSON.stringify(player));
-        clonePlayer.tetromino=rotate(clonePlayer.tetromino,dir);
-
-
-
-        const pos=clonePlayer.pos.x;
-        let offset=1;
-        while(checkCollision(clonePlayer,stage,{x:0,y:0}))
-        {
-            clonePlayer.pos.x+=offset;
-            offset-=(offset+(offset>0?1:-1));
-             if(offset>clonePlayer.tetromino[0].length){
-                 rotate(clonePlayer.tetromino,-dir);
-                 clonePlayer.pos.x=pos;
-                 return ;
-             }
-
-        }
-
-        setPlayer(clonePlayer);
-    }
-     
-
-    const updatePlayerPos=({x,y,collided})=>{
-        setPlayer(prev=>({
-            ...prev,
-            pos:{x:(prev.pos.x+=x),y:(prev.pos.y+=y)},
-            collided,
-        }))
-    }
-    const resetPlayer=useCallback(()=>{
-        setPlayer({
-            pos:{x:STAGE_WIDTH/2-2,y:0},
-            tetromino:randomTetromino().shape,
-            collided:false,
-             
-    })
-},[])
-     
-
-    return [player,updatePlayerPos,resetPlayer,playerRotate];
-    
-}
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
+};
